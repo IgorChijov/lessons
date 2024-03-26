626cafb4f82fcf7db25606b11dabf40e506d53f7
IgorChijov <igorchijof@gmail.com>
2024-03-26 21:30:38 +0300

    Изменил приветствие

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.print("Кукусики!");
+        System.out.print("Привет! Проверьте моё домашнее задание :)");
 
     }
 }
