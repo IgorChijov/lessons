b6bda6e6c36c047d906d5739551b7076dfd36014
IgorChijov <igorchijof@gmail.com>
2024-03-26 19:34:55 +0300

    Initial commit

---
+++ src/english/Hello.java
@@ -0,0 +1,9 @@
+package english;
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+
+
+    }
+}
---
+++ src/russian/Hello.java
@@ -0,0 +1,9 @@
+package russian;
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+
+
+    }
+}
