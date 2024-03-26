0757e3bf0ca7c4f79fbe652fda082864d6babc64
IgorChijov <igorchijof@gmail.com>
2024-03-26 20:53:56 +0300

    Приветствие на русском и английском языке

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.print("Кукусики!");
+        System.out.println("Кукусики!");
+        System.out.println("Hello hello!");
 
     }
 }
