0d81b8ae9ca250e9cb00bfeae6f397b653bee3f4
IgorChijov <igorchijof@gmail.com>
2024-04-30 23:56:07 +0300

    условные операторы

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
             if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
                 System.out.println("Год является високосным");
             } else {
-                System.out.println("Год не является високосным");
+                System.out.println("Год не високосный");
             }
         }
 
